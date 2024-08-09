affb97bc83c3e2b638b7d3687cb1aa9d9a9fef78
azik <irongidra@gmail.com>
2024-08-09 12:08:55 +0900

    addfiles

---
+++ fish/config.fish
@@ -0,0 +1,86 @@
+ # АЛИАСЫ
+clear
+
+alias c='clear'
+alias da='doas'
+alias sudo='doas'
+alias ls='ls --color=auto'
+alias md='mkdir -p'
+alias sys='sudo systemctl'
+alias fuc='fish_update_completions'
+alias x='exit'
+
+alias grubmk='doas grub-mkconfig -o /boot/grub/grub.cfg'
+
+alias pi='doas pacman -S --noconfirm'
+alias pr='doas pacman -R --noconfirm'
+alias prr='doas pacman -Rss --noconfirm'
+alias prc='sudo pacman -R $(pacman -Qdtq)'
+
+alias fc='micro ~/.config/fish/config.fish'
+
+alias hdc='micro ~/.config/hypr/hyprland.conf'
+alias hrc='micro ~/.config/hypr/rules.conf'
+alias hbc='micro ~/.config/hypr/binds.conf'
+alias hac='micro ~/.config/hypr/auto.conf'
+
+alias sdc='micro ~/.config/sway/config'
+alias sac='micro ~/.config/sway/auto'
+alias sbc='micro ~/.config/sway/binds'
+alias src='micro ~/.config/sway/rules'
+
+alias kc='micro ~/.config/kitty/kitty.conf'
+
+alias rdc='micro ~/.config/ranger/rc.conf'
+alias rrc='micro ~/.config/ranger/rifle.conf'
+
+alias gitc='git clone'
+
+alias ca='cava'
+alias pm='pulsemixer'
+
+alias r='ranger'
+alias l='lf'
+alias j='joshuto'
+alias j='joshuto'
+
+alias hy='hyprctl'
+alias hyc='hyprctl clients'
+
+alias appid='swaymsg -t get_tree | grep "app_id"'
+alias class='swaymsg -t get_tree | grep "class"'
+
+alias m='micro'
+alias n='nvim'
+alias h='helix'
+
+alias mk='make'
+alias mki='sudo make install'
+
+alias lut='lutgen apply -p catppuccin-macchiato'
+
+alias dots='cd ~/.dots/arch-dots/'
+alias gp='git commit -m addfiles & git push'
+alias ga='git add .'
+
+# ЭКСПОРТЫ
+set fish_greeting
+export MOZ_ENABLE_WAYLAND=1
+export MICRO_TRUECOLOR=1 
+export WLR_NO_HARDWARE_CURSORS=1
+export RANGER_LOAD_DEFAULT_RC=false
+
+export TERM=kitty
+export TERMINAL=kitty
+export EDITOR=micro
+
+export dots=~/.config
+export cfg=~/.config
+export swaycfg=~/.config/sway
+
+fish_add_path ~/.spicetify
+fish_add_path ~/.local/bin
+zoxide init fish | source
+
+[ "$(tty)" = "/dev/tty1" ] && exec Hyprland
+# [ "$(tty)" = "/dev/tty1" ] && exec sway
---
+++ micro/backups/%home%azik%mint-y-theme%1-change-color.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+# changing the assets from colour
+
+# Mint-Y Theme 
+# Light green - 9ab87c
+# Dark green - 8fa876
+# but there are some minor green variations to consider as well
+
+##################################################################################################################
+##################################################################################################################
+##################################################################################################################
+# put your colour codes in here and do not change the rest
+# Never put a hashtag or # in front of the colour code if copy/pasting from gpick
+# Never put "FF" at the end if copy/pasting from inkscape
+# Always 6 letters or numbers
+# THESE CODES MUST BE CHANGED
+# THESE CODES MUST BE CHANGED
+# THESE CODES MUST BE CHANGED
+# THESE CODES MUST BE CHANGED
+
+
+#original light version is #9ab87c
+personallightcolour=8bd5ca
+
+#original dark version is #8fa876
+personaldarkcolour=8bd5ca
+
+
+# THESE CODES MUST BE CHANGED
+# THESE CODES MUST BE CHANGED
+# THESE CODES MUST BE CHANGED
+# THESE CODES MUST BE CHANGED
+##################################################################################################################
+##################################################################################################################
+##################################################################################################################
+
+
+echo "All colours in the folder src are still the original green ones from github!!!"
+echo "Choose your colour that will replace the green online or with tools like gpick."
+echo "e.g. sudo apt-get install gpick"
+echo "Changing ....."
+echo "Wait for it ...."
+
+##################################################################################################################
+
+## DO NOT CHANGE THESE LINES
+## DO NOT CHANGE THESE LINES
+## DO NOT CHANGE THESE LINES
+
+
+#original light version is #9ab87c
+oldcolour1=9ab87c
+#original dark version is #8fa876
+oldcolour2=8fa876
+#other colour variations in svg
+oldcolour3=779559
+oldcolour4=9abe76
+oldcolour5=9ab87d
+oldcolour6=88a66a
+oldcolour7=81a65b
+
+
+newcolour1=$personallightcolour
+newcolour2=$personaldarkcolour
+
+newcolour3=$personaldarkcolour
+newcolour4=$personallightcolour
+newcolour5=$personallightcolour
+newcolour6=$personallightcolour
+newcolour7=$personaldarkcolour	
+
+
+find src -name "*.*" -type f -exec sed -i 's/'$oldcolour1'/'$newcolour1'/g' {}  \;
+find src -name "*.*" -type f -exec sed -i 's/'$oldcolour2'/'$newcolour2'/g' {}  \;
+find src -name "*.*" -type f -exec sed -i 's/'$oldcolour3'/'$newcolour3'/g' {}  \;
+find src -name "*.*" -type f -exec sed -i 's/'$oldcolour4'/'$newcolour4'/g' {}  \;
+find src -name "*.*" -type f -exec sed -i 's/'$oldcolour5'/'$newcolour5'/g' {}  \;
+find src -name "*.*" -type f -exec sed -i 's/'$oldcolour6'/'$newcolour6'/g' {}  \;
+find src -name "*.*" -type f -exec sed -i 's/'$oldcolour7'/'$newcolour7'/g' {}  \;
+
+
+#Rubberband Solution for now
+
+#border
+oldcolour1=76905b
+find src -type f -exec sed -i 's/'$oldcolour1'/'$newcolour1'/g' {}  \;
+
+#background of rubberband is in rgba
+#from hex to decimal
+redhex=${newcolour1:0:2}
+greenhex=${newcolour1:2:2}
+bluehex=${newcolour1:4:2}
+
+reddec=$((16#$redhex))
+greendec=$((16#$greenhex))
+bluedec=$((16#$bluehex))
+
+rgbacolour="$reddec, $greendec, $bluedec"
+
+#old rgba colour of background rubberband
+oldcolour1="118, 144, 91"
+newcolour1=$rgbacolour
+
+#gtk-dark rubberband
+find src -type f -exec sed -i "s/$oldcolour1/$newcolour1/g" {}  \;
+
+#gtk + gtk-darker rubberband
+oldcolour1="129, 166, 91"
+find src -type f -exec sed -i "s/$oldcolour1/$newcolour1/g" {}  \;
+
+
+echo "All css files but also svg files will be affected"
+echo "PNG'S will not be altered with this script"
+echo
+echo "Next up delete all assets i.e. png's with script number 2"
